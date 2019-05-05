99c5b95320a3a2858bbce3283ed02ec580bcac4c
T0EY <toeiu@api.com>
2019-05-05 16:24:10 +0700

    Category OK

--- app/src/main/java/api/dev/apifood/MyConstant.java
+++ app/src/main/java/api/dev/apifood/MyConstant.java
@@ -2,7 +2,7 @@ package api.dev.apifood;
 
 public class MyConstant {
 
-    private String urlCategory = "http://apiinnovation.dyndns.org:4567/Service_AdaRes.svc/GetTableAll";
+    private String urlCategory = "http://apiinnovation.dyndns.org:4567/Service_AdaRes.svc/GetProductType";
 
     public String getUrlCategory() {
         return urlCategory;
---
+++ app/src/main/java/api/dev/apifood/OnClickItem.java
@@ -0,0 +1,9 @@
+package api.dev.apifood;
+
+import android.view.View;
+
+public interface OnClickItem {
+
+    public void onClickItem(View view,int position);
+
+}
