7b0494d6fc9ed5ba7a9c7095653d98bf935e6053
T0EY <toeiu@api.com>
2019-05-05 11:16:46 +0700

    Initial commit

---
+++ app/src/main/java/api/dev/apifood/MainActivity.java
@@ -0,0 +1,23 @@
+package api.dev.apifood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment,new ListCategoryFragment())
+                    .commit();
+        }
+
+    } // Main Method
+
+} // Main Class
